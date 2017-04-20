3f126c0d2f1a98e4df2e6db78d1f9409aadb8d2a
Dale <drcorns@hotmail.com>
2017-04-20 09:28:10 -0700

    disable everything but static assets for intitial testing

--- index.js
+++ index.js
@@ -7,37 +7,37 @@
 'use strict';
 const express = require('express');
 const bodyParser = require('body-parser');
-const corngoose = require('corngoose');
+//const corngoose = require('corngoose');
 const app = express();
 app.use(bodyParser.json());
 //const port = process.env.PORT || 3000;
-corngoose.startDB('monopoly');
+//corngoose.startDB('monopoly');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/allPrizeData', function(req, res){
-  corngoose.getCollection('prizes', function(err, data){
-    if (err){
-      return null;
-    }
-    res.status(200);
-    res.contentType = 'json';
-    res.send(data);
-  });
-});
-
-app.post('/updatePrize', function(req, res){
-  corngoose.dbDocUpdate({name: req.body.name, value: req.body.value},req.body,'prizes',function(err, data){
-    if (err){
-      res.status(400);
-      res.contentType = 'json';
-      res.send(err);
-      return null;
-    }
-    res.status(200);
-    res.contentType = 'json';
-    res.send(data);
-  });
-});
+// app.get('/allPrizeData', function(req, res){
+//   corngoose.getCollection('prizes', function(err, data){
+//     if (err){
+//       return null;
+//     }
+//     res.status(200);
+//     res.contentType = 'json';
+//     res.send(data);
+//   });
+// });
+//
+// app.post('/updatePrize', function(req, res){
+//   corngoose.dbDocUpdate({name: req.body.name, value: req.body.value},req.body,'prizes',function(err, data){
+//     if (err){
+//       res.status(400);
+//       res.contentType = 'json';
+//       res.send(err);
+//       return null;
+//     }
+//     res.status(200);
+//     res.contentType = 'json';
+//     res.send(data);
+//   });
+// });
 
 // const server = app.listen(port, 'localhost', function(){
 //   console.log('Server listening on port ' + port);
