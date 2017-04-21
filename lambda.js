181ea68ad808fd38d010e27664c56da6f82c3495
Dale <drcorns@hotmail.com>
2017-04-21 12:42:20 -0700

    change settings attempting success

--- lambda.js
+++ lambda.js
@@ -1,6 +1,6 @@
 'use strict'
 const awsServerlessExpress = require('aws-serverless-express')
-const app = require('./app')
+const app = require('./index')
 // NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
 // due to a compressed response (e.g. gzip) which has not been handled correctly
 // by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
